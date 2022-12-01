742e25e6822d90e506a356609a22f0d1bf67fcab
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-01 08:01:05 +0100

    Day1 Part2 solution

--- day1/__main__.py
+++ day1/__main__.py
@@ -24,5 +24,15 @@ def part1(input_fn: str):
     return max(sum_inventories(split_inventories(file_lines(input_fn))))
 
 
+def part2(input_fn: str):
+    return sum(
+        sorted(sum_inventories(split_inventories(file_lines(input_fn))), reverse=True)[
+            0:3
+        ]
+    )
+
+
 print(f"Part1 Sample: {part1('day1/sample')}")
 print(f"Part1: {part1('day1/input')}")
+print(f"Part2 Sample: {part2('day1/sample')}")
+print(f"Part2: {part2('day1/input')}")
