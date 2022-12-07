8bceaca01c6ca33100dc7fb0877013f90c4841df
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-07 09:56:37 +0100

    Day7 part2

--- day7/__main__.py
+++ day7/__main__.py
@@ -57,14 +57,18 @@ def size(dir: Directory) -> int:
     )
 
 
+def dir_sizes(dir: Directory) -> Generator[Tuple[Directory, int], None, None]:
+    yield (dir, size(dir))
+    for subdir in dir.dirs.values():
+        yield from dir_sizes(subdir)
+
+
 def small_dirs(
     dir: Directory, max_size: int = 100000
 ) -> Generator[Tuple[Directory, int], None, None]:
-    for subdir in dir.dirs.values():
-        yield from small_dirs(subdir)
-        subdir_size = size(subdir)
-        if subdir_size <= max_size:
-            yield (subdir, subdir_size)
+    for dir, size in dir_sizes(dir):
+        if size <= max_size:
+            yield (dir, size)
 
 
 def part1(file_name: str):
@@ -73,11 +77,34 @@ def part1(file_name: str):
     )
 
 
+def print_tree(dir: Directory, indent: int = 0, recursive: bool = True):
+    print((" " * indent) + f"- {dir.name} (size: {size(dir)})")
+    for file in dir.files.values():
+        print((" " * (indent + 2)) + f"| {file.name} (size: {file.size})")
+    for dir in dir.dirs.values():
+        if recursive:
+            print_tree(dir, indent + 2, True)
+        else:
+            print((" " * (indent + 2)) + f"- {dir.name} (size: {size(dir)})")
+
+
 def part2(file_name: str):
-    pass
+    total_size = 70000000
+    total_needed = 30000000
+
+    root = directory_structure(file_lines(file_name))
+
+    size_of_root = size(root)
+
+    free_space = total_size - size_of_root
+    needed = total_needed - free_space
+
+    for (dir, s) in sorted(dir_sizes(root), key=lambda e: e[1]):
+        if s >= needed:
+            return s
 
 
 print(f"Part1 Sample: {part1('day7/sample')}")
 print(f"Part1: {part1('day7/input')}")
-# print(f"Part2 Sample: {part2('day7/sample')}")
-# print(f"Part2: {part2('day7/input')}")
+print(f"Part2 Sample: {part2('day7/sample')}")
+print(f"Part2: {part2('day7/input')}")
