03652c713dfe623a85ef6582118cba29a55b1107
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-09 09:54:17 +0100

    Day9 part2

--- day9/__main__.py
+++ day9/__main__.py
@@ -1,6 +1,6 @@
-from typing import Generator, Tuple
+from typing import Dict, Generator, Set, Tuple
 
-from utils import file_lines
+from utils import draw_coordinates, file_lines
 
 
 def move_tail(head_pos: Tuple[int, int], tail_pos: Tuple[int, int]) -> Tuple[int, int]:
@@ -28,35 +28,68 @@ def move_head(head_pos: Tuple[int, int], dir: str) -> Tuple[int, int]:
     }[dir]
 
 
-def rope_head_tail() -> Generator[Tuple[int, int], str, None]:
+def rope_head_segment() -> Generator[
+    Tuple[Tuple[int, int], Tuple[int, int]], str, None
+]:
     head_pos = (0, 0)
     tail_pos = (0, 0)
 
     while True:
-        dir = yield tail_pos
+        dir = yield (head_pos, tail_pos)
 
         head_pos = move_head(head_pos, dir)
         tail_pos = move_tail(head_pos, tail_pos)
 
 
 def part1(fn: str):
-    gen = rope_head_tail()
-    tail_pos = next(gen)
-    tail_poss = { tail_pos }
+    gen = rope_head_segment()
+    tail_pos = next(gen)[1]
+    tail_poss = {tail_pos}
     for move in file_lines(fn):
         (dir, count) = move.split(" ")
         for _ in range(int(count)):
-            tail_pos = gen.send(dir)
+            tail_pos = gen.send(dir)[1]
             tail_poss.add(tail_pos)
     return len(tail_poss)
 
 
+def rope_segment() -> Generator[
+    Tuple[Tuple[int, int], Tuple[int, int]], Tuple[int, int], None
+]:
+    head_pos = (0, 0)
+    tail_pos = (0, 0)
+
+    while True:
+        head_pos = yield (head_pos, tail_pos)
+        tail_pos = move_tail(head_pos, tail_pos)
+
 
 def part2(fn: str):
-    file_lines(fn)
+    head = rope_head_segment()
+    next(head)
+    segments = [rope_segment() for i in range(9)]
+    [next(segment) for segment in segments]
+    tail_poss: Set[Tuple[int, int]] = {(0, 0)}
+    for move in file_lines(fn):
+        (dir, count) = move.split(" ")
+        for i in range(int(count)):
+            coords: Dict[Tuple[int, int], str] = {(0, 0): "s"}
+            head_head, head_tail = head.send(dir)
+            next_segment_head = head_tail
+            coords[head_head] = "H"
+            for index, segment in enumerate(segments):
+                segment_head, segment_tail = segment.send(next_segment_head)
+                coords[segment_head] = f"{index + 1}"
+                next_segment_head = segment_tail
+                if index + 1 == 9:
+                    tail_poss.add(segment_head)
+    #            if i == int(count) - 1:
+    #                print(draw_coordinates(coords))
+    return len(tail_poss)
 
 
 print(f"Part1 Sample: {part1('day9/sample')}")
 print(f"Part1: {part1('day9/input')}")
-#print(f"Part2 Sample: {part2('day9/sample')}")
-#print(f"Part2: {part2('day9/input')}")
+print(f"Part2 Sample: {part2('day9/sample')}")
+print(f"Part2 Sample2: {part2('day9/sample2')}")
+print(f"Part2: {part2('day9/input')}")
