5420347ba33c0580032cdbfcbf38d552150bd19d
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-11 15:37:11 +0100

    Day11 part2

--- day11/README.md
+++ day11/README.md
@@ -3,3 +3,6 @@
 [Advent of Code 2022 - Day 11](https://adventofcode.com/2022/day/11) Solution
 
 # Notes
+
+Divisors are relative primes, keeping calculations in the modulo space based on the product of all divisors
+limits the growth of the numbers.
--- day11/__main__.py
+++ day11/__main__.py
@@ -32,7 +32,7 @@ def parse_true_dest(line: str) -> int:
     return int(drop_prefix(line, "    If true: throw to monkey "))
 
 
-def Parse_false_dest(line: str) -> int:
+def parse_false_dest(line: str) -> int:
     return int(drop_prefix(line, "    If false: throw to monkey "))
 
 
@@ -45,9 +45,15 @@ class Monkey:
     false_dest: int
     inspected: int = 0
 
-    def round(self, monkeys: List["Monkey"]) -> List["Monkey"]:
+    def round(
+        self, monkeys: List["Monkey"], mod_base: int, dampen: bool = True
+    ) -> List["Monkey"]:
         for item in self.items:
-            new_worry = int(math.floor(self.operation(item) / 3))
+            new_worry = (
+                int(math.floor(self.operation(item) / 3))
+                if dampen
+                else self.operation(item)
+            ) % mod_base
             monkeys[
                 (self.true_dest if (new_worry % self.divisor == 0) else self.false_dest)
             ].items.append(new_worry)
@@ -62,37 +68,44 @@ def parse_monkey(lines: List[str]) -> Monkey:
         operation=parse_operation(lines[2]),
         divisor=parse_divisor(lines[3]),
         true_dest=parse_true_dest(lines[4]),
-        false_dest=Parse_false_dest(lines[5]),
+        false_dest=parse_false_dest(lines[5]),
     )
 
 
 def print_round(i: int, monkeys: List[Monkey]):
     print(f"After round {i}, the monkeys are holding items with these worry levels:")
     for index, monkey in enumerate(monkeys):
-        print(f"Monkey {index}: {monkey.items}")
+        print(f"Monkey {index}: {len(monkey.items)}")
 
 
-def part1(fn: str):
+def monkey_business(fn: str, rounds: int, dampen: bool) -> int:
     monkeys = [
         parse_monkey(monkey_lines)
         for monkey_lines in match_into_chunks(
             file_lines(fn), lambda l: l == "", return_match=False
         )
     ]
+    mod_base = math.prod(m.divisor for m in monkeys)
     # print_round(0, monkeys)
-    for round_no in range(20):
+    for round_no in range(rounds):
         for monkey in monkeys:
-            monkey.round(monkeys)
+            monkey.round(monkeys, mod_base, dampen=dampen)
         # print_round(round_no + 1, monkeys)
-    inspecteds = sorted([m.inspected for m in monkeys], reverse=True)[:2]
-    return math.prod(inspecteds)
+    inspecteds = sorted([m.inspected for m in monkeys], reverse=True)
+    # print(inspecteds)
+    top_inspecteds = inspecteds[:2]
+    return math.prod(top_inspecteds)
+
+
+def part1(fn: str):
+    return monkey_business(fn, rounds=20, dampen=True)
 
 
 def part2(fn: str):
-    pass
+    return monkey_business(fn, rounds=10000, dampen=False)
 
 
 print(f"Part1 Sample: {part1('day11/sample')}")
 print(f"Part1: {part1('day11/input')}")
-# print(f"Part2 Sample: {part2('day11/sample')}")
-# print(f"Part2: {part2('day11/input')}")
+print(f"Part2 Sample: {part2('day11/sample')}")
+print(f"Part2: {part2('day11/input')}")
