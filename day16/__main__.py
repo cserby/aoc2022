1fcf6a31c7a0d70067c4d27f676d55d9b973f01b
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-17 08:57:24 +0100

    Day16 part2 off by 2

--- day16/__main__.py
+++ day16/__main__.py
@@ -30,7 +30,9 @@ def parse_cave(fn: str) -> Tuple[Dict[str, int], Dict[str, bool], Dict[str, List
     return (flow_rate, valve_on, connections)
 
 
-def distances(connections: Dict[str, List[str]], flow_rate: Dict[str, int]) -> Dict[str, Dict[str, int]]:
+def distances(
+    connections: Dict[str, List[str]], flow_rate: Dict[str, int]
+) -> Dict[str, Dict[str, int]]:
     # https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
 
     vertices = list(sorted(connections.keys()))
@@ -63,7 +65,8 @@ def distances(connections: Dict[str, List[str]], flow_rate: Dict[str, int]) -> D
     return {
         vert: {
             other: int(dsts[vert_idx][other_idx])
-            for other_idx, other in enumerate(vertices) if flow_rate[other] > 0
+            for other_idx, other in enumerate(vertices)
+            if flow_rate[other] > 0
         }
         for (vert_idx, vert) in enumerate(vertices)
     }
@@ -120,121 +123,91 @@ def part1(fn: str) -> int:
     return visit("AA", flow_rate, valve_on, dsts)
 
 
-def visit2(
-    state_1: Tuple[
-        str, int
-    ],  # 1 is on his way to [0], and will get there when time_left will be [1]
-    state_2: Tuple[
-        str, int
-    ],  # 2 is on his way to [0], and will get there when time_left will be [1]
+def max_release(
+    pos1: str,
+    time_left1: int,
+    pos2: str,
+    time_left2: int,
+    simulation_time_left: int,
     flow_rate: Dict[str, int],
     valve_on: Dict[str, bool],
     distances: Dict[str, Dict[str, int]],
-    time_left: int = 26,  # Simulation clock
-    indent: int = 0,
 ) -> int:
-    # print(" " * indent + f"-- Minute {26 - time_left + 1} --")
-    # print(" " * indent + f"Valves {[ k for k,v in valve_on.items() if v]} are open")
-    released_if_we_take_this_route = 0
-
-    (pos1, at1) = state_1
-    new_at1 = at1
-    (pos2, at2) = state_2
-    new_at2 = at2
+    if simulation_time_left < 0:
+        return 0
 
     def step_of_1() -> bool:
-        return time_left == at1
+        return simulation_time_left == time_left1
 
     def step_of_2() -> bool:
-        return time_left == at2
+        return simulation_time_left == time_left2
 
-    if step_of_1() and flow_rate[pos1] > 0:
-        # 1 opens valve
-        new_at1 -= 1
-        assert not valve_on[pos1]
-        curr_release = flow_rate[pos1] * new_at1
-        # print(" " * indent + f"You open valve {pos1}, will release {curr_release}")
-        released_if_we_take_this_route += curr_release
-        valve_on[pos1] = True
+    release = 0
 
-    if step_of_2() and flow_rate[pos2] > 0:
-        # 2 opens valve
-        new_at2 -= 1
-        assert not valve_on[pos2]
-        curr_release = flow_rate[pos2] * new_at2
-        # print(" " * indent + f"Elephant opens valve {pos2}, will release {curr_release}")
-        released_if_we_take_this_route += curr_release
-        valve_on[pos2] = True
+    if step_of_1():
+        if flow_rate[pos1] > 0:
+            assert not valve_on[pos1]
+            valve_on[pos1] = True
+            release += flow_rate[pos1] * time_left1
+        new_poses1 = [ pos for pos in distances[pos1].keys() if (pos != pos2) and (not valve_on[pos]) ]
+    else:
+        new_poses1 = [pos1]
 
-    max_release = None
-    for new_pos1 in (
-        [
-            possible_new_valve
-            for possible_new_valve in distances[pos1].keys()
-            if (
-                flow_rate[possible_new_valve] > 0  # No reason to go to a place where's no valve
-                and (not valve_on[possible_new_valve])  # The valve is not on yet
-                and distances[pos1][possible_new_valve] + 2
-                <= new_at1  # We have time to get there, and open the valve
-            )
-        ]
-        if step_of_1()
-        else [pos1]  # If it's not our turn, we keep our target position
-    ):
-        for new_pos2 in (
-            [
-                possible_new_valve
-                for possible_new_valve in distances[pos2].keys()
-                if (
-                    flow_rate[possible_new_valve] > 0
-                    and (not valve_on[possible_new_valve])
-                    and distances[pos2][possible_new_valve] + 2 <= new_at2
-                )
-            ]
-            if step_of_2()
-            else [pos2]
-        ):
-            # Skip if selection would give suboptimal results
-            if (
-                (new_pos1 == new_pos2)  # We select the same target in the same round
-                or (
-                    new_pos1 == pos2
-                )  # We select the target the other is already on his way to
-                or (new_pos2 == pos1)
-            ):
+    if step_of_2():
+        if flow_rate[pos2] > 0:
+            assert not valve_on[pos2]
+            valve_on[pos2] = True
+            release += flow_rate[pos2] * time_left2
+        new_poses2 = [ pos for pos in distances[pos2].keys() if (pos != pos1) and (not valve_on[pos]) ]
+    else:
+        new_poses2 = [pos2]
+
+    further_max_rel = 0
+
+    for new_pos1 in new_poses1:
+        for new_pos2 in new_poses2:
+            if new_pos1 == new_pos2:
                 continue
 
-            max_release_if_we_go_this_way = visit2(
-                (new_pos1, new_at1 - distances[pos1][new_pos1]),
-                (new_pos2, new_at2 - distances[pos2][new_pos2]),
+            new_time_left1 = time_left1 - distances[pos1][new_pos1] - (1 if step_of_1() else 0) # Go there, open valve
+            new_time_left2 = time_left2 - distances[pos2][new_pos2] - (1 if step_of_2() else 0)
+
+            new_simulation_time_left = max(new_time_left1, new_time_left2)
+
+            possible_further_max_rel = max_release(
+                new_pos1,
+                new_time_left1,
+                new_pos2,
+                new_time_left2,
+                new_simulation_time_left,
                 flow_rate,
                 valve_on.copy(),
-                distances,
-                max(
-                    new_at1 - distances[pos1][new_pos1],
-                    new_at2 - distances[pos2][new_pos2],
-                ),
-                indent + 2,
+                distances
             )
 
-            if max_release is None or max_release_if_we_go_this_way > max_release:
-                max_release = max_release_if_we_go_this_way
+            if further_max_rel is None or possible_further_max_rel > further_max_rel:
+                further_max_rel = possible_further_max_rel
 
-    if max_release is not None:
-        released_if_we_take_this_route += max_release
-
-    # print(" " * indent + f"This option results in a total release of {released_if_we_take_this_route}")
-    return released_if_we_take_this_route
+    return release + further_max_rel
 
 
 def part2(fn: str) -> int:
     (flow_rate, valve_on, connections) = parse_cave(fn)
     dsts = distances(connections, flow_rate)
-    return visit2(("AA", 26), ("AA", 26), flow_rate, valve_on, dsts)
+    return max_release(
+        "AA",
+        26,
+        "AA",
+        26,
+        26,
+        flow_rate,
+        valve_on,
+        dsts,
+    )
 
 
-# print(f"Part1 Sample: {part1('day16/sample')}")
-# print(f"Part1: {part1('day16/input')}")
+print(f"Part1 Sample: {part1('day16/sample')}")
+print(f"Part1: {part1('day16/input')}")
 print(f"Part2 Sample: {part2('day16/sample')}")
 # cProfile.run("print(part1('day16/input'))")
 # print(f"Part2: {part2('day16/input')}")
