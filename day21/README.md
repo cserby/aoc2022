cb9e681dc8a504d708558334988225a0db214b7d
Marton Cserbak <marton.cserbak@airtame.com>
2022-12-21 12:57:16 +0100

    Day21 part2

--- day21/README.md
+++ day21/README.md
@@ -3,3 +3,5 @@
 [Advent of Code 2022 - Day 21](https://adventofcode.com/2022/day/21) Solution
 
 # Notes
+
+Solved part2 by using successive approximation while plotting diff between root's op1 and op2.
--- day21/__main__.py
+++ day21/__main__.py
@@ -1,20 +1,20 @@
-from typing import Callable, Dict, Iterator, List, Optional
+from typing import Callable, Dict, List, Optional
 
 from utils import file_lines
 
 
 class Monkey:
     name: str
-    _shout: Optional[int]
+    _shout: Optional[float]
     operand: Optional[str]
-    _op1: Optional[int]
-    _op2: Optional[int]
-    observers: List[Callable[[int], None]]
+    _op1: Optional[float]
+    _op2: Optional[float]
+    observers: List[Callable[[float], None]]
 
     def __init__(
         self,
         name: str,
-        shout: Optional[int],
+        shout: Optional[float],
         operand: Optional[str],
         op1_monkey: Optional["Monkey"],
         op2_monkey: Optional["Monkey"],
@@ -32,39 +32,35 @@ class Monkey:
             self.op2_monkey = op2_monkey
 
     def _perform_operation(self) -> None:
-        self.shout = eval(f"int({self._op1} {self.operand} {self._op2})")
-        print(
-            f"Monkey {self.name} performs operation {self._op1} {self.operand} {self._op2}, and gets {self.shout}"
-        )
+        self.shout = eval(f"{self._op1} {self.operand} {self._op2}")
 
     @property
-    def op1(self) -> Optional[int]:
+    def op1(self) -> Optional[float]:
         return self._op1
 
     @op1.setter
-    def op1(self, op1: int) -> None:
+    def op1(self, op1: float) -> None:
         self._op1 = op1
         if self._op2 is not None:
             self._perform_operation()
 
     @property
-    def op2(self) -> Optional[int]:
+    def op2(self) -> Optional[float]:
         return self._op2
 
     @op2.setter
-    def op2(self, op2: int) -> None:
+    def op2(self, op2: float) -> None:
         self._op2 = op2
         if self._op1 is not None:
             self._perform_operation()
 
     @property
-    def shout(self) -> Optional[int]:
+    def shout(self) -> Optional[float]:
         return self._shout
 
     @shout.setter
-    def shout(self, shout: int) -> None:
+    def shout(self, shout: float) -> None:
         self._shout = shout
-        print(f"Monkey {self.name} shouts {shout}")
         for notify in self.observers:
             notify(shout)
 
@@ -78,8 +74,7 @@ class Monkey:
             self.op1 = monkey.shout
         else:
 
-            def set_op1(op1: int) -> None:
-                print(f"Monkey {self.name} hears {monkey.name} shout {op1} (op1)")
+            def set_op1(op1: float) -> None:
                 self.op1 = op1
 
             monkey.observers.append(set_op1)
@@ -94,8 +89,7 @@ class Monkey:
             self.op2 = monkey.shout
         else:
 
-            def set_op2(op2: int) -> None:
-                print(f"Monkey {self.name} hears {monkey.name} shout {op2} (op2)")
+            def set_op2(op2: float) -> None:
                 self.op2 = op2
 
             monkey.observers.append(set_op2)
@@ -104,34 +98,35 @@ class Monkey:
     def parse(line: str, monkeys: Dict[str, "Monkey"]) -> Dict[str, "Monkey"]:
         (name, value) = line.split(": ")
 
-        def get_monkey(name: str) -> Monkey:
-            try:
-                return monkeys[name]
-            except KeyError:
-                monkeys[name] = Monkey(
-                    name=name,
-                    shout=None,
-                    operand=None,
-                    op1_monkey=None,
-                    op2_monkey=None,
-                )
-                return monkeys[name]
-
-        monkey = get_monkey(name)
+        monkey = get_monkey(name, monkeys)
 
         try:
-            shout = int(value)
+            shout = float(value)
             monkey.shout = shout
         except ValueError:
             (op1, operand, op2) = value.split(" ")
             monkey.operand = operand
-            monkey.op1_monkey = get_monkey(op1)
-            monkey.op2_monkey = get_monkey(op2)
+            monkey.op1_monkey = get_monkey(op1, monkeys)
+            monkey.op2_monkey = get_monkey(op2, monkeys)
 
         return monkeys
 
     def __repr__(self) -> str:
         return f"Monkey(name={self.name}, shout={self.shout}, operation={'?' if self.op1 is None else self.op1} {'?' if self.operand is None else self.operand} {'?' if self.op2 is None else self.op2}, observers={len(self.observers)}"
+
+
+def get_monkey(name: str, monkeys: Dict[str, Monkey]) -> Monkey:
+    try:
+        return monkeys[name]
+    except KeyError:
+        monkeys[name] = Monkey(
+            name=name,
+            shout=None,
+            operand=None,
+            op1_monkey=None,
+            op2_monkey=None,
+        )
+        return monkeys[name]
 
 
 def part1(fn: str) -> int:
@@ -143,14 +138,50 @@ def part1(fn: str) -> int:
     assert "root" in monkeys.keys()
     root_shout = monkeys["root"].shout
     assert root_shout is not None
-    return root_shout
+    return int(root_shout)
 
 
-def part2(fn: str) -> int:
-    raise NotImplementedError()
+class RootMonkey(Monkey):
+    def __init__(
+        self, op1_monkey: Optional["Monkey"], op2_monkey: Optional["Monkey"]
+    ) -> None:
+        super().__init__("root", None, None, None, None)
+
+    def _perform_operation(self) -> None:
+        assert self._op1 == self.op2
+
+
+def part2(fn: str) -> float:
+    humn_shout = 3.349136384441e12
+
+    monkeys: Dict[str, Monkey] = dict()
+
+    root: Optional[Monkey] = None
+    humn: Optional[Monkey] = None
+    for l in file_lines(fn):
+        if l.startswith("root"):
+            (name, value) = l.split(": ")
+            (op1_monkey, _, op2_monkey) = value.split(" ")
+            root = get_monkey("root", monkeys)
+            root.__class__ = RootMonkey
+            root.op1_monkey = get_monkey(op1_monkey, monkeys)
+            root.op2_monkey = get_monkey(op2_monkey, monkeys)
+            continue
+        if l.startswith("humn"):
+            humn = get_monkey("humn", monkeys)
+            continue
+
+        Monkey.parse(l, monkeys)
+
+    assert root is not None
+    assert humn is not None
+
+    humn.shout = humn_shout
+
+    return int(humn_shout)
 
 
 print(f"Part1 Sample: {part1('day21/sample')}")
 print(f"Part1: {part1('day21/input')}")
 # print(f"Part2 Sample: {part2('day21/sample')}")
-# print(f"Part2: {part2('day21/input')}")
+print(f"Part2: {part2('day21/input')}")
